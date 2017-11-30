ba8b1ee90f2331fc93dbec37c6bf67fff2c70c9b
Adam Patruno <AdamPatruno@titlesource.com>
2017-11-30 11:19:34 -0500

    Changing port for port contention

--- app.js
+++ app.js
@@ -13,4 +13,4 @@ app.post('/postTechTalkMessage', (req,res)=>{
     res.send(req.body.message);
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3004, () => console.log('Example app listening on port 3004!'))
