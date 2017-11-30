04afc80df833baf5bcadbd5cc7670d22d4a1ff4c
Adam Patruno <AdamPatruno@titlesource.com>
2017-11-30 08:03:47 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const  bodyParser = require('body-parser')
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+app.get('/getTechTalkMessage', (req, res) => res.send('Hello Tech Talk'))
+
+app.post('/postTechTalkMessage', (req,res)=>{
+    res.send(req.body.message);
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
